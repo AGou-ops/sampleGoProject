8c7102212b37905fa51c7bec2d531bedf7d6ddcf
AGou-ops <suofeiyaxx@gmail.com>
2022-08-04 10:39:50 +0800

    Add formatJson_byUA.

---
+++ greenlight_project/cmd/api/formatJson_byUA.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"regexp"
+)
+
+// > User-Agent: curl/7.80.0
+
+func writeJson(req *http.Request, data interface{}) (string, error)  {
+	client_ua := req.UserAgent()
+	result, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return "",errors.New("indent failed")
+	}
+	isCurlUA, _ := regexp.MatchString("curl.*", client_ua)
+
+	if isCurlUA {
+		return string(result), nil
+	}
+
+	return "", errors.New("not curl UA")
+}
